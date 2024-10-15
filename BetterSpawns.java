c2fec3316240a0cd3f1b5844439380504f68e418
rexyiscool <165537304+r3xyi4cool@users.noreply.github.com>
2024-10-15 19:03:21 +0530

    Update and rename SpawnPluginBlitz.java to BetterSpawns.java

--- BetterSpawns.java
+++ BetterSpawns.java
@@ -13,7 +13,46 @@ public final class SpawnPluginBlitz extends JavaPlugin {
     private int teleportDelay;
     private String setspawnPermission;
     private String reloadPermission;
+    @Ovpackage me.rexyiscool.betterSpawns;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterSpawns extends JavaPlugin {
+
+    public int teleportDelay;
+    public String setspawnPermission;
+    public String reloadPermission;
+    public int teleportTaskId;
+    public int movementCheckTaskId;
+
     @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        reloadConfig();
+        if (getConfig() == null) {
+            getLogger().severe("Config file is missing!");
+        } else {
+            getLogger().info("Config file loaded successfully.");
+        }
+
+        getCommand("setspawn").setExecutor(new SetSpawnCommand(this));
+        getCommand("spawn").setExecutor(new SpawnCommand(this));
+        getCommand("spawnreload").setExecutor(new ReloadCommand(this));
+
+        reloadPermission = getConfig().getString("spawn-reload");
+        teleportDelay = getConfig().getInt("teleport-delay");
+        setspawnPermission = getConfig().getString("setspawnperm");
+        getLogger().info("The Better Spawns Plugin is enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Better Spawns plugin has been shutdown successfully");
+    }
+}
+
+erride
     public void onEnable() {
         saveDefaultConfig();
         reloadConfig();

20421f168402d2a23402f854f5392c1c81265a9a
rexyiscool <165537304+r3xyi4cool@users.noreply.github.com>
2024-10-16 22:55:44 +0530

    Update BetterSpawns.java
    Intergrated with the menu command

--- com.rexyiscool.betterspawn/BetterSpawns.java
+++ com.rexyiscool.betterspawn/BetterSpawns.java
@@ -1,7 +1,12 @@
 package me.rexyiscool.betterSpawns;
 
+import me.rexyiscool.betterSpawns.commands.SpawnMenuCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class BetterSpawns extends JavaPlugin {
 
@@ -10,6 +15,9 @@ public final class BetterSpawns extends JavaPlugin {
     public String reloadPermission;
     public int teleportTaskId;
     public int movementCheckTaskId;
+
+    private final Map<Player, ChatColor> playerColorMap = new HashMap<>();
+    private ColorSelectionGUI colorSelectionGUI;
 
     @Override
     public void onEnable() {
@@ -21,19 +29,35 @@ public final class BetterSpawns extends JavaPlugin {
             getLogger().info("Config file loaded successfully.");
         }
 
-        getCommand("setspawn").setExecutor(new SetSpawnCommand(this));
-        getCommand("spawn").setExecutor(new SpawnCommand(this));
+        getCommand("bsetspawn").setExecutor(new SetSpawnCommand(this));
+        getCommand("bspawn").setExecutor(new SpawnCommand(this));
         getCommand("spawnreload").setExecutor(new ReloadCommand(this));
+        getCommand("spawnmenu").setExecutor(new SpawnMenuCommand(this));  // Register the spawnmenu command
 
         reloadPermission = getConfig().getString("spawn-reload");
         teleportDelay = getConfig().getInt("teleport-delay");
         setspawnPermission = getConfig().getString("setspawnperm");
+
+        colorSelectionGUI = new ColorSelectionGUI(this);
+        Bukkit.getPluginManager().registerEvents(colorSelectionGUI, this);
+
         getLogger().info("The Better Spawns Plugin is enabled");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Better Spawns plugin has been shutdown successfully");
+        getLogger().info("Better Spawns plugin has been shut down successfully");
+    }
+
+    public void setPlayerColor(Player player, ChatColor color) {
+        playerColorMap.put(player, color);
+    }
+
+    public ChatColor getPlayerColor(Player player) {
+        return playerColorMap.getOrDefault(player, ChatColor.WHITE);  // Default to white if no color is set
+    }
+
+    public ColorSelectionGUI getColorSelectionGUI() {
+        return colorSelectionGUI;
     }
 }
-

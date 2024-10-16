e3958ab91aa4a76de887bc6292dbd528c2a547ba
rexyiscool <165537304+r3xyi4cool@users.noreply.github.com>
2024-10-16 22:56:29 +0530

    Update SpawnCommand.java
    Intergrated with the SpawnMenu

--- com.rexyiscool.betterspawn/SpawnCommand.java
+++ com.rexyiscool.betterspawn/SpawnCommand.java
@@ -1,6 +1,7 @@
 package me.rexyiscool.betterSpawns;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,11 +9,15 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SpawnCommand implements CommandExecutor {
 
     private final BetterSpawns plugin;
+    private final Map<Player, Integer> teleportTasks = new HashMap<>();  // Store teleport task per player
+    private final Map<Player, Integer> movementCheckTasks = new HashMap<>();  // Store movement check task per player
 
     public SpawnCommand(BetterSpawns plugin) {
         this.plugin = plugin;
@@ -23,22 +28,31 @@ public class SpawnCommand implements CommandExecutor {
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
             Location initialLocation = player.getLocation();
-            player.sendMessage("Teleporting in " + plugin.teleportDelay + " seconds...");
+
+
+            ChatColor playerColor = plugin.getPlayerColor(player);
+            player.sendMessage(playerColor + "Teleporting in " + plugin.teleportDelay + " seconds...");
+
 
             BukkitTask movementCheckTask = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
                 if (player.getLocation().distanceSquared(initialLocation) > 0.01) {
-                    player.sendMessage("Teleportation canceled due to movement.");
-                    Bukkit.getScheduler().cancelTask(plugin.teleportTaskId);
-                    Bukkit.getScheduler().cancelTask(plugin.movementCheckTaskId);
+                    player.sendMessage(playerColor + "Teleportation canceled due to movement.");
+                    Bukkit.getScheduler().cancelTask(teleportTasks.get(player));
+                    Bukkit.getScheduler().cancelTask(movementCheckTasks.get(player));
+                    teleportTasks.remove(player);
+                    movementCheckTasks.remove(player);
                 }
             }, 0L, 5L);
-            plugin.movementCheckTaskId = movementCheckTask.getTaskId();
+            movementCheckTasks.put(player, movementCheckTask.getTaskId());
+
 
             BukkitTask teleportTask = Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                Bukkit.getScheduler().cancelTask(plugin.movementCheckTaskId);
+                Bukkit.getScheduler().cancelTask(movementCheckTasks.get(player));
+                movementCheckTasks.remove(player);
 
                 player.teleport(player.getWorld().getSpawnLocation());
-                player.sendMessage("Teleporting to spawn...");
+                player.sendMessage(playerColor + "Teleporting to spawn...");
+
 
                 List<String> commandsToExecute = plugin.getConfig().getStringList("command.command");
                 if (commandsToExecute != null && !commandsToExecute.isEmpty()) {
@@ -47,13 +61,17 @@ public class SpawnCommand implements CommandExecutor {
                         plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), processedCommand);
                     }
                 } else {
-                    player.sendMessage("No commands to execute after teleport.");
+                    player.sendMessage(playerColor + "No commands to execute after teleport.");
                 }
+
+                teleportTasks.remove(player);
+
             }, plugin.teleportDelay * 20);
-            plugin.teleportTaskId = teleportTask.getTaskId();
+            teleportTasks.put(player, teleportTask.getTaskId());
+
             return true;
         } else {
-            commandSender.sendMessage("This command can only be done by the player");
+            commandSender.sendMessage("This command can only be executed by a player.");
         }
 
         return false;
